fd79b8d4970f6e1554bcf0c4b3c35d1b2f0b4359
wrobell <wrobell@pld-linux.org>
2017-03-14 20:23:20 +0000

    - implemented track map rendering with geotiler library

---
+++ antrak/bc/map.py
@@ -0,0 +1,71 @@
+#
+# AnTrak - Activity and location data analysis
+#
+# Copyright (C) 2017 by Artur Wroblewski <wrobell@riseup.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import asyncio
+import cairocffi as cairo
+import functools
+import geotiler
+import logging
+import math
+import redis
+from geotiler.cache import redis_downloader
+from antrak.dao import map as map_dao
+from antrak.db import tx
+
+logger = logging.getLogger(__name__)
+
+FMT_MAP_FILENAME = '{0[start]:%Y-%m-%d} - {0[trip]} - {0[name]}.png'.format
+ALPHA = 0.5
+RADIUS = 1
+
+@tx
+async def render(dev, query, provider, size):
+    client = redis.Redis('localhost')
+    downloader = redis_downloader(client)
+    render = functools.partial(render_map, downloader, provider, size)
+
+    data = await map_dao.load_pos(dev, query)
+    tasks = (
+        render(item['positions'], item['extent'], FMT_MAP_FILENAME(item))
+        for item in data
+    )
+    return (await asyncio.gather(*tasks))
+
+async def render_map(downloader, provider, size, positions, extent, output):
+    logger.debug('rendering map {}: {}'.format(output, extent))
+    mm = geotiler.Map(size=size, extent=extent, provider=provider)
+    img = await geotiler.render_map_async(mm, downloader=downloader)
+
+    buff = bytearray(img.convert('RGBA').tobytes('raw', 'BGRA'))
+    surface = cairo.ImageSurface.create_for_data(
+        buff, cairo.FORMAT_ARGB32, *size
+    )
+    cr = cairo.Context(surface)
+
+    points = (mm.rev_geocode(p) for p in positions)
+    for x, y in points:
+        cr.set_source_rgba(1.0, 0.0, 0.0, ALPHA)
+        cr.arc(x, y, RADIUS, 0, 2 * math.pi)
+        cr.fill()
+        cr.stroke()
+
+    surface.write_to_png(output)
+    logger.debug('written {}'.format(output))
+
+# vim: sw=4:et:ai
---
+++ antrak/dao/map.py
@@ -0,0 +1,42 @@
+#
+# AnTrak - Activity and location data analysis
+#
+# Copyright (C) 2017 by Artur Wroblewski <wrobell@riseup.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from antrak.db import tx
+
+SQL_LOAD_POS = """
+select t.trip, t.name, t.start,
+    array[
+        min(st_x(p.location)), min(st_y(p.location)),
+        max(st_x(p.location)), max(st_y(p.location))
+    ] as extent,
+    array_agg(array[st_x(p.location), st_y(p.location)]) as positions
+from track t
+    inner join position p on t.device = p.device
+        and p.timestamp between t.start and t.end
+where p.device = $1 and t.trip || ' ' || t.name ~* $2
+group by t.trip, t.name, t.start
+order by t.trip, t.start
+"""
+
+@tx
+async def load_pos(dev, query):
+    data = await tx.conn.fetch(SQL_LOAD_POS, dev, query)
+    return data
+
+# vim: sw=4:et:ai
